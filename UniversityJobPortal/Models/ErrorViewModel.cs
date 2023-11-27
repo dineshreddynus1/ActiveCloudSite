9d5c1741e082756446b8f97b93e2b74220bef799
dineshreddynus1 <145508377+dineshreddynus1@users.noreply.github.com>
2023-11-27 15:51:02 -0500

    Delete UniversityJobPortal directory

--- UniversityJobPortal/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace UniversityJobPortal.Models
-{
-	public class ErrorViewModel
-	{
-		public string? RequestId { get; set; }
-
-		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-	}
-}
