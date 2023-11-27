96b302801afae99cf6ac3526d4e3f796e02f2170
Your Name <abhirockz56@gmail.com>
2023-11-27 18:04:44 -0500

    This is Abhijeet

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace UNIVERSITYJOBSPORTAL.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Models/JobModel.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+
+namespace UNIVERSITYJOBSPORTAL.Models
+{
+    public class JobModel
+    {
+        public int Id { get; set; }
+
+
+        public string title { get; set; }
+
+        public string description { get; set; }
+
+
+        public string type { get; set; }
+
+        public string location { get; set; }
+
+        public string salary { get; set; }
+
+        public int positions { get; set; }
+
+        public int departmentId { get; set; }
+
+        public virtual List<JobApplicantModel> JobApplicants { get; set; }
+
+        public DepartmentModel department { get; set; }
+
+    }
+
+
+    public class JobModelConfiguration : IEntityTypeConfiguration<JobModel>
+    {
+        public void Configure(EntityTypeBuilder<JobModel> builder)
+        {
+            builder.HasKey(x => new { x.Id });
+
+            builder.HasOne(x => x.department).WithMany(x => x.jobs).HasForeignKey(x => x.departmentId).OnDelete(DeleteBehavior.ClientSetNull);
+         }
+
+    }
+}
