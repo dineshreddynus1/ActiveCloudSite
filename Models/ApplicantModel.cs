ef1d57c883c00d054ca0fe526326dbad1fc856c5
Your Name <abhirockz56@gmail.com>
2023-11-27 16:13:57 -0500

    This is Abhijeet

---
+++ Models/ApplicantModel.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+
+namespace UNIVERSITYJOBSPORTAL.Models
+{
+    public class ApplicantModel
+    {
+        public int applicantId { get; set; }
+
+        public string name { get; set; }
+
+        public string phone { get; set; }
+
+        public string email { get; set; }
+
+        public string coverLetter { get; set; }
+
+        public string univID { get; set; }
+
+        public virtual List<JobApplicantModel> JobApplicants { get; set; }
+    }
+
+
+    public class ApplicantModelConfiguration : IEntityTypeConfiguration<ApplicantModel>
+    {
+        public void Configure(EntityTypeBuilder<ApplicantModel> builder)
+        {
+            builder.HasKey(x => new { x.applicantId });
+
+
+        }
+    }
+}
---
+++ Models/DepartmentModel.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+
+namespace UNIVERSITYJOBSPORTAL.Models
+{
+    public class DepartmentModel
+    {
+        public int Id { get; set; } 
+
+        public string Name { get; set; }
+
+        public string email { get; set; }
+
+        public virtual List<JobModel> jobs { get; set; }
+
+
+    }
+
+    public class DepartmentModelConfiguration : IEntityTypeConfiguration<DepartmentModel>
+    {
+        public void Configure(EntityTypeBuilder<DepartmentModel> builder)
+        {
+            builder.HasKey(x => new { x.Id });
+            
+
+        }
+    }
+}
